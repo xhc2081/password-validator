14d9c32560851d8b91f62fefbacc9b0f4d27d7aa
xhc2081 <xhc2081@163.com>
2017-05-27 19:18:17 -0300

    stage2

--- app/src/test/java/com/example/xuhongcheng/a2/ExampleUnitTest.java
+++ app/src/test/java/com/example/xuhongcheng/a2/ExampleUnitTest.java
@@ -17,17 +17,26 @@ public class ExampleUnitTest {
 
     @Test
     public void testName() {
-        stage1 s1 = new stage1("password");
-        stage1 s2 = new stage1("password1");
-        stage1 s3 = new stage1("p");
-        stage1 s4 = new stage1("passwordpassword");
-        stage1 s5 = new stage1("passwor");
+        stage1 s1 = new stage1("1234567sS");
+        stage1 s2 = new stage1("password");
+        stage1 s3 = new stage1("passworD");
+        stage1 s4 = new stage1("passworD1");
+        stage1 s5 = new stage1("ssworD1");
+        stage1 s6 = new stage1("password1");
+        stage1 s7 = new stage1("pass1");
+        stage1 s8 = new stage1("a");
+        stage1 s9 = new stage1("1234567sS-'`.'.;;]");
 
-        assertEquals(s1.pass(), 1);
+        assertEquals(s1.pass(), 5);
         assertEquals(s2.pass(), 2);
-        assertEquals(s3.pass(), 1);
-        assertEquals(s4.pass(), 2);
-        assertEquals(s5.pass(), 1);
+        assertEquals(s3.pass(), 4);
+        assertEquals(s4.pass(), 5);
+        assertEquals(s5.pass(), 4);
+        assertEquals(s6.pass(), 4);
+        assertEquals(s7.pass(), 3);
+        assertEquals(s8.pass(), 1);
+        assertEquals(s9.pass(), 5);
+
 
     }
 }
